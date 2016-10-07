41fd7457956468147986fc86040e1fff08ac925e
yukicode <yolandaapply@gmail.com>
2016-10-07 14:06:55 -0700

    Add project template
    Add html content and css style for project template.
    Add newer jquery (required by bootstrap js components).
    Add projects contents.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,25 @@
 var bio = {
-    name: ko.observable("XUERU MCMULLAN"),
-    title: ko.observable("FRONT-END WEB DEVELOPER"),
-
+    name: "XUERU MCMULLAN",
+    title: "FRONT-END WEB DEVELOPER",
+    summary: "Front-End Web Developer specializing in JavaScript and JS frameworks. Experienced in building single-page web applications that are mobile and user-friendly.",
 };
+
+var projects = [
+    {
+        name: "Neighborhood Map",
+        images: ["./images/neighborhood-map-01.jpg"],
+        descrition: [
+            "Neighborhood Map is a single-page, responsive web application that helps people location appartments in Seattle and nearby area. Users could search for apartments based on name or desired location. Apartments can be sorted by name or price range. The app provides detailed apartment informations including Google and Yelp reviews as well as apartments' contact information.",
+            "The application uses Knockout and jQuery in the front-end; Node and Express in the back-end. Front-end and back-end communites through a restful API. Building process was automated using Gulp."
+        ],
+        link: 'https://github.com/yukicode/UDA-front-end-program',
+        demoLink: '',
+    }
+];
 
 var viewModel = {
     bio : bio,
+    projects: projects,
 };
 
 ko.applyBindings(viewModel);
